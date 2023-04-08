5bc35f7b459fd76cea458bba81b932abf380b6d0
tanishkasinghal <tanishka2306@gmail.com>
2023-04-09 01:48:30 +0530

    JWT Authentication implemented

--- demo/src/main/java/com/speMajor/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/speMajor/demo/config/SecurityConfig.java
@@ -2,11 +2,12 @@ package com.speMajor.demo.config;
 
 
 import com.speMajor.demo.security.CustomUserDetailService;
-//import com.speMajor.demo.security.JwtAuthenticationEntryPoint;
-//import com.speMajor.demo.security.JwtAuthenticationFilter;
+import com.speMajor.demo.security.JwtAuthenticationEntryPoint;
+import com.speMajor.demo.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -20,31 +21,33 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+//@EnableWebMvc
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class  SecurityConfig {
 
     @Autowired
     private CustomUserDetailService customUserDetailService;
-//
-//    @Autowired
-//    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//
-//    @Autowired
-//    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf().
-                disable().
-                authorizeHttpRequests()
-                //.requestMatchers("/api/v1/auth/login").permitAll()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest()
                 .authenticated().and()
-                //.exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
-                //.and()
-                //.sessionManagement()
-                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                .httpBasic();
-       // http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //.httpBasic();
+        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
@@ -59,10 +62,10 @@ public class  SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
-//        return configuration.getAuthenticationManager();
-//    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
 }
--- demo/src/main/java/com/speMajor/demo/controller/AuthController.java
+++ demo/src/main/java/com/speMajor/demo/controller/AuthController.java
@@ -1,43 +1,56 @@
-//package com.speMajor.demo.controller;
-//
-//import com.speMajor.demo.payload.JwtAuthRequest;
-//import com.speMajor.demo.payload.JwtAuthResponse;
-//import com.speMajor.demo.security.JwtTokenHelper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1/auth")
-//public class AuthController {
-//    @Autowired
-//    private JwtTokenHelper jwtTokenHelper;
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//    @PostMapping("/login")
-//    public ResponseEntity<JwtAuthResponse> createToken(@RequestBody JwtAuthRequest request){
-//        this.authenticate(request.getUsername(),request.getPassword());
-//        UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
-//        String token = this.jwtTokenHelper.generateToken(userDetails);
-//
-//        JwtAuthResponse response = new JwtAuthResponse();
-//        response.setToken(token);
-//       // response.setUser(this.mapper.map((User) userDetails, UserDto.class));
-//        return new ResponseEntity<JwtAuthResponse>(response, HttpStatus.OK);
-//    }
-//
-//    private void authenticate(String username, String password) {
-//        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(username,password);
-//        this.authenticationManager.authenticate(authenticationToken);
-//    }
-//}
+package com.speMajor.demo.controller;
+import com.speMajor.demo.payload.JwtAuthRequest;
+import com.speMajor.demo.payload.JwtAuthResponse;
+import com.speMajor.demo.security.JwtTokenHelper;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final JwtTokenHelper jwtTokenHelper;
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(JwtTokenHelper jwtTokenHelper, UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
+        this.jwtTokenHelper = jwtTokenHelper;
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> createToken(@NotNull @RequestBody JwtAuthRequest request) throws Exception {
+        System.out.println("Inside the controller");
+        this.authenticate(request.getUsername(),request.getPassword());
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
+        System.out.println("Spring user details "+userDetails);
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        System.out.println("Token"+token);
+        JwtAuthResponse response = new JwtAuthResponse();
+        response.setToken(token);
+       // response.setUser(this.mapper.map((User) userDetails, UserDto.class));
+        return new ResponseEntity<JwtAuthResponse>(response, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(username,password);
+        try{
+            this.authenticationManager.authenticate(authenticationToken);
+        }catch (BadCredentialsException e){
+            System.out.println("Invalid Credentials !!");
+            throw new Exception("Invalid Username or Password !!");
+        }
+
+    }
+}
