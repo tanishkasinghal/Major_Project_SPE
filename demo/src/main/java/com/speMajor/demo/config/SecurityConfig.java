5ed46163c56d89e3826ddefdb44ca71b0aaf913f
tanishkasinghal <tanishka2306@gmail.com>
2023-04-08 15:45:40 +0530

    Basic authentication

--- demo/src/main/java/com/speMajor/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/speMajor/demo/config/SecurityConfig.java
@@ -1,27 +1,27 @@
-//package com.speMajor.demo.config;
-//
-//
+package com.speMajor.demo.config;
+
+
 //import com.speMajor.demo.security.CustomUserDetailService;
 //import com.speMajor.demo.security.JwtAuthenticationEntryPoint;
 //import com.speMajor.demo.security.JwtAuthenticationFilter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
 //    @Autowired
 //    private CustomUserDetailService customUserDetailService;
 //
@@ -30,19 +30,23 @@
 //
 //    @Autowired
 //    private JwtAuthenticationFilter jwtAuthenticationFilter;
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        http.csrf().disable().authorizeHttpRequests()
-//                .requestMatchers("/api/v1/auth/login").permitAll()
-//                .anyRequest().authenticated().and()
-//                .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().
+                disable().
+                authorizeHttpRequests()
+                //.requestMatchers("/api/v1/auth/login").permitAll()
+                .anyRequest()
+                .authenticated().and()
+                //.exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                //.and()
+                //.sessionManagement()
+                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .httpBasic();
+       // http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
@@ -60,5 +64,5 @@
 //    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
 //        return configuration.getAuthenticationManager();
 //    }
-//
-//}
+
+}
